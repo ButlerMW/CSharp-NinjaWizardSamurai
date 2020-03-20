59b4355fb80053d5d7bf88d4ade6ae7fa7619dd3
Michael Butler <57372823+ButlerMW@users.noreply.github.com>
2020-03-20 10:03:15 -0600

    Fix syntax errors

--- NinjaWizardSamurai/Samurai.cs
+++ NinjaWizardSamurai/Samurai.cs
@@ -6,29 +6,29 @@ namespace NinjaWizardSamurai
 
         public Samurai(string name) : base(name)
         {
-            Health = 200;
+            health = 200;
         }
 
         public override int Attack(Human target)
         {
             Console.WriteLine($"{ Name } is attacking { target.Name }!");
-            if (target.Health < 50)
+            if (target.health < 50)
             {
                 Console.WriteLine($"Instant Kill!!!");
-                target.Health = 0;
+                target.health = 0;
             }
             else
             {
                 base.Attack(target); // calls the base attack 
             }
-            return target.Health;
+            return target.health;
         }
 
         public int Meditate() 
         {
             Console.WriteLine($"{ Name } is meditating and back to full health!");
-            Health = 200;
-            return Health;
+            health = 200;
+            return health;
         }
     }
 }
