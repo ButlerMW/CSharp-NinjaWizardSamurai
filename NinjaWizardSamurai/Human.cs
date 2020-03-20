e08895d9118ba55490b37e08e50d2b95bf0efe32
Michael Butler <57372823+ButlerMW@users.noreply.github.com>
2020-03-19 19:55:34 -0600

    Clean up and fixing small errors

--- NinjaWizardSamurai/Human.cs
+++ NinjaWizardSamurai/Human.cs
@@ -1,14 +1,21 @@
 using System;
+
 namespace NinjaWizardSamurai
 {
+    // Create a Human class with four public feilds: Name, Strength, Intelligence, Dexterity
     public class Human
     {
+        // Fields for Human
         public string Name;
         public int Strength;
         public int Intelligence;
         public int Dexterity;
+        // Add an additional private field for health, and a public property to access or "get" health
         public int Health {get;set;}
 
+        
+        
+        // Add a constructor method that takes a string to initialize Name - and that will initialize Strength, Intelligence, and Dexterity to a default value, 3 and health to default value of 100
         public Human(string name)
         {
             Name = name;
@@ -18,6 +25,7 @@ namespace NinjaWizardSamurai
             Health = 100;
         }
 
+        // Let's create an additional constructor that accepts 5 parameters, so we can set custom values for every field
         public Human(string name, int strength, int intelligence, int dexterity, int health)
         {
             Name = name;
@@ -26,14 +34,15 @@ namespace NinjaWizardSamurai
             Dexterity = dexterity;
             Health = health;
         }
-
+        
+        // Now add a method called Attack, which when invoked, should reduce the health of a Human object that is passed as a parameter. The damage done should be 5 * strength. This method should return the remaining health of the target object.
         public virtual int Attack(Human target)
         {
-            target.Health -= 5 * Strength;
-            Console.WriteLine($"{target.Name} loses {5 * Strength} Health!");
+            int Damage = 5 * Strength;
+            target.Health -= Damage;
+            Console.WriteLine($"{target.Name} loses {Damage} Health!");
+            Console.WriteLine($"{target.Name} has {target.Health} Health left!");
             return target.Health;
         }
-
-
     }
 }
