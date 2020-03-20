23d83e99d98e1c2eacb6f3d3b1c06fd44f870af9
Michael Butler <57372823+ButlerMW@users.noreply.github.com>
2020-03-20 10:02:03 -0600

    Fix syntax errors

--- NinjaWizardSamurai/Program.cs
+++ NinjaWizardSamurai/Program.cs
@@ -6,35 +6,26 @@ namespace NinjaWizardSamurai
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fight!");
             Human Joe = new Human("Joe");
             Wizard Merlin = new Wizard("Merlin");
             Ninja Koga = new Ninja("Koga");
             Samurai Jack = new Samurai("Jack");
-            // Joe.Attack(Merlin);
-            // Console.WriteLine($"Joe HP: {Joe.Health}"); 
-            // Console.WriteLine($"Merlin Int: {Merlin.Intelligence}");
-            // Merlin.Attack(Koga);
             Koga.Attack(Merlin);
-            Console.WriteLine($"Koga HP: {Koga.Health}");
-            Console.WriteLine($"Jack HP: {Jack.Health}");
-            Console.WriteLine($"Merlin HP: {Merlin.Health}");
+            Console.WriteLine($"Koga HP: {Koga.health}");
+            Console.WriteLine($"Jack HP: {Jack.health}");
+            Console.WriteLine($"Merlin HP: {Merlin.health}");
             Jack.Attack(Merlin);
-            Console.WriteLine($"Merlin HP: {Merlin.Health}");
+            Console.WriteLine($"Merlin HP: {Merlin.health}");
             Merlin.Heal(Merlin);
-            Console.WriteLine($"Merlin HP: {Merlin.Health}");
+            Console.WriteLine($"Merlin HP: {Merlin.health}");
             Merlin.Attack(Jack);
-            Console.WriteLine($"Jack HP: {Jack.Health}");
+            Console.WriteLine($"Jack HP: {Jack.health}");
             Jack.Meditate();
-            Console.WriteLine($"Jack HP: {Jack.Health}");
+            Console.WriteLine($"Jack HP: {Jack.health}");
             Koga.Steal(Jack);
-            Console.WriteLine($"Jack HP: {Jack.Health}");
-            Console.WriteLine($"Koga HP: {Koga.Health}");
-
-
-
-
-
+            Console.WriteLine($"Jack HP: {Jack.health}");
+            Console.WriteLine($"Koga HP: {Koga.health}");
         }
     }
 }
